a28705d42a6bc0061a9d6ebd9c4f33fb41a5c684
김태현 <allone1129@gmail.com>
2020-04-06 00:45:59 +0900

    Implement ITransmittable

--- SimpleSocketServerLibrary/ITransmittable.cs
+++ SimpleSocketServerLibrary/ITransmittable.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace SimpleSocketServerLibrary
 {
-    interface ITransmittable
+    public interface ITransmittable
     {
+        int NetworkID { get; set; }
+
+        void Write(OutputMemoryStream outputMemoryStream);
+        void Write(OutputMemoryBitStream outputMemoryBitStream);
+
+        void Read(InputMemoryStream inputMemoryStream);
+        void Read(InputMemoryBitStream inputMemoryBitStream);
     }
 }
